c6c47a6e08689f5dc870332a777323f2c3989a55
Rozstone <wangsiteng123@gmail.com>
2022-01-15 11:13:36 +0800

    add scf_bootstrap

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+	env: {
+		STATIC_URL: isProd ? process.env.STATIC_URL : "",
+	},
+	assetPrefix: isProd ? process.env.STATIC_URL : "",
+	reactStrictMode: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
+
+module.exports = nextConfig;
