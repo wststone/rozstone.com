8eb35bb081a7f97110d3324bf9b46f433c8c4c33
Rozstone <wangsiteng123@gmail.com>
2022-03-21 19:14:41 +0800

    route loading

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import Router from "next/router";
+import Head from "next/head";
 import nextI18NextConfig from "@root/next-i18next.config.js";
 import "@styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />;
+	const [loading, setLoading] = useState(true);
+	Router.events.on("routeChangeStart", () => setLoading(true));
+	Router.events.on("routeChangeComplete", () => setLoading(false));
+	return (
+		<>
+			{loading && (
+				<Head>
+					<title>读取中</title>
+					<link
+						rel="icon"
+						href="/favicon/routing.gif"
+						type="image/gif"
+					/>
+				</Head>
+			)}
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default appWithTranslation(MyApp, nextI18NextConfig);
